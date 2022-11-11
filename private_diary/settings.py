71159fd632d0b8a2f3e1fb17a3b9ad124300b801
masamMatsu <masaya.matsumiya@mail.o-hara.ac.jp>
2022-11-11 10:52:49 +0900

    settings.py email

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -14,7 +14,8 @@ MEDIA_ROOT = '/usr/share/nginx/html/media'
 # Amazon SES関連設定
 AWS_SES_ACCESS_KEY_ID = os.environ.get('AWS_SES_ACCESS_KEY_ID')
 AWS_SES_SECRET_ACCESS_KEY = os.environ.get('AWS_SES_SECRET_ACCESS_KEY')
-EMAIL_BACKEND = 'django_ses.SESBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = '/home/app_admin/log'
 
 # ロギング
 LOGGING = {
